c877eba7496dc66bb332ef49b233a104cac9f388
ollehmichael <66451149+ollehmichael@users.noreply.github.com>
2021-07-27 17:28:21 +0900

    fix: footer refactoring

--- src/components/FooterContainer.tsx
+++ src/components/FooterContainer.tsx
@@ -1,41 +1,21 @@
 import React from "react";
-import { Container, Grid, Header, List, Segment } from "semantic-ui-react";
+import { List } from "semantic-ui-react";
 
-const Footer = () => (
-  <Segment inverted vertical style={{ padding: "5em 0em" }}>
-    <Container>
-      <Grid divided inverted stackable>
-        <Grid.Row>
-          <Grid.Column width={3}>
-            <Header inverted as="h4" content="Social Media" />
-            <List link inverted>
-              <List.Item as="a">Instagram</List.Item>
-              <List.Item as="a">LinkedIn</List.Item>
-              <List.Item as="a">GitHub</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column width={3}>
-            <Header inverted as="h4" content="Contacts" />
-            <List link inverted>
-              <List.Item as="a">Reach Me</List.Item>
-              <List.Item as="a">Resume / CV</List.Item>
-              <List.Item as="a">Request for a Meeting </List.Item>
-              <List.Item as="a">Request for a Workout Session</List.Item>
-            </List>
-          </Grid.Column>
-          {/* <Grid.Column width={7}>
-            <Header as="h4" inverted />
-            <p>
-              I made this website for fun
-              <br />I can make better ones for sure
-              <br />
-              Hit me up and I'll show you
-            </p>
-          </Grid.Column> */}
-        </Grid.Row>
-      </Grid>
-    </Container>
-  </Segment>
-);
+type FooterProps = {
+  as: string;
+  link: string;
+  title: string;
+};
 
-export default Footer;
+const FooterContainer: React.FC<FooterProps> = (props) => {
+  const { as, link, title } = props;
+  return (
+    <div>
+      <List.Item as={as} onClick={() => window.open(link, "_blank")}>
+        {title}
+      </List.Item>
+    </div>
+  );
+};
+
+export default FooterContainer;
